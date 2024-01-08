8226147822cfc820d7c9867ff622611925c121cf
root <root@haxinja>
2024-01-08 22:58:16 +0800

    Working main

--- Final Main/main.py
+++ Final Main/main.py
@@ -7,7 +7,7 @@ from windows.dashb_page import Ui_MainWindow as Dashboard
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.session_type = 1
+        self.session_type = 0
         self.user_sessions = {
             0 : 'User not logged',
             1: 'User is logged'
@@ -20,6 +20,9 @@ class MainWindow(QMainWindow):
         elif(self.session_type == 1):
             self.ui = Dashboard()
         self.ui.setupUi(self)
+    
+    def on_click(self):
+        print("YAWAKA CHRISTIAN!")
 
 
 if(__name__ == "__main__"):
