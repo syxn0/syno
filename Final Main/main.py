9c642727766267d9cd03728683a7bd1bf3de7c0e
abalesluke <abalesluke@gmail.com>
2024-01-09 05:34:49 +0800

    db functions added

--- Final Main/main.py
+++ Final Main/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, sqlite3
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from windows.login_page import Ui_MainWindow as Login
@@ -7,12 +7,24 @@ from windows.dashb_page import Ui_MainWindow as Dashboard
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.db_conn = sqlite3.connect("data.db")
+        self.sql = self.db_conn.cursor()
+        self.sql.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, user_type INTEGER NOT NULL)")
+        self.db_conn.commit()
+
         self.session_type = 0
         self.user_sessions = {
             0 : 'User not logged',
-            1: 'User is logged'
+            1 : 'User is logged'
         }
         self.session()
+
+    def __userExists(self, username, password):
+        self.sql.execute("SELECT * FROM users WHERE username = ? AND password = ?;",(username, password))
+        results = self.sql.fetchall()
+        if(len(results) > 0):
+            return True
+        return False
 
     def show_message(self, title, text, icon_type):
         msg = QMessageBox()
@@ -32,7 +44,8 @@ class MainWindow(QMainWindow):
     def on_login_pressed(self):
         username = self.ui.user.text()
         password = self.ui.password.text()
-        if(username == 'cj' and password == '1234'):
+        isValid = self.__userExists(username, password)
+        if(isValid):
             self.session_type = 1
             self.session()
         else:
